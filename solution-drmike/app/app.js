24717f8df0ca95ea6f6c716590de110b98cce847
Dr. Mike Hopper <drmikeh@gmail.com>
2016-08-15 23:42:56 -0400

    Minor tweaks.

--- solution-drmike/app/app.js
+++ solution-drmike/app/app.js
@@ -15,14 +15,13 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 // uncomment after placing your favicon in /public
-// app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-// Routes
 app.use('/', routes);
 app.use('/users', users);
 
