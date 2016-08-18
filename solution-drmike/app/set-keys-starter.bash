0a50f314f7104d0970e79e08470b3581d9721a53
Dr. Mike Hopper <drmikeh@gmail.com>
2016-08-18 12:27:24 -0400

    Added set-keys-starter.bash

---
+++ solution-drmike/app/set-keys-starter.bash
@@ -0,0 +1,6 @@
+# TODO: add your TWITTER keys here
+# See https://apps.twitter.com/
+export TWITTER_CONSUMER_KEY=''
+export TWITTER_CONSUMER_SECRET=''
+export TWITTER_ACCESS_TOKEN=''
+export TWITTER_ACCESS_TOKEN_SECRET=''
